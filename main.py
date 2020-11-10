fa7f20a72356ec3c9278defd4d6656b44da103a2
Yudabin <hi1793@naver.com>
2020-11-10 09:48:58 +0900

    모델 학습, 예측

---
+++ main.py
@@ -0,0 +1,70 @@
+import pickle
+
+import flask
+from flask import Flask, request, render_template
+# from sklearn.externals import joblib
+import numpy as np
+from scipy import misc
+from tensorflow.keras.models import load_model
+from keras_preprocessing.text import Tokenizer
+from tensorflow.keras.preprocessing.sequence import pad_sequences
+from konlpy.tag import Okt;
+
+
+
+t = Okt()
+okt = Okt()
+
+app = Flask(__name__)
+
+
+# 메인 페이지 라우팅
+@app.route("/")
+@app.route("/index")
+def index():
+    return flask.render_template('index.html')
+
+
+# 데이터 예측 처리
+@app.route('/predict', methods=['POST'])
+
+# print("\n 테스트 정확도: %.4f" % (loaded_model.evaluate(X_test, y_test)[1]))
+
+def make_prediction():
+    if request.method == 'POST':
+        new_sentence = '엄청 재밌어요'
+        stopwords = ['의', '가', '이', '은', '들', '는', '좀', '잘', '걍', '과', '도',
+                     '를', '으로', '자', '에', '와', '한', '하다']
+        max_len = 30
+
+        with open('./ml/tokenizer.pickle', 'rb') as f:
+            tokenizer = pickle.load(f)
+
+        # tokenizer = Tokenizer(12725, oov_token = 'OOV') # pickle 사용하기
+        # tokenizer.fit_on_texts(new_sentence)
+
+        new_sentence = okt.morphs(new_sentence, stem=True) # 토큰화
+        new_sentence = [word for word in new_sentence if not word in stopwords] # 불용어 제거
+        encoded = tokenizer.texts_to_sequences([new_sentence]) # 정수 인코딩
+        pad_new = pad_sequences(encoded, maxlen = max_len) # 패딩
+        score = float(loaded_model.predict(pad_new)) # 예측
+        if(score > 0.5):
+            label = (score * 100)
+            result = '긍정'
+        else:
+            label = (1 - score) * 100
+            result = '부정'
+        # 결과 리턴
+        return render_template('index.html', label=label,result=result)
+
+# sentiment_predict('이 영화 개꿀잼 ㅋㅋㅋ')
+
+
+if __name__ == '__main__':
+    # 모델 로드
+    # ml/model.py 선 실행 후 생성
+    # model = joblib.load('./model/model.pkl')
+    loaded_model = load_model('./model/best_model.h5')
+
+    # Flask 서비스 스타트
+    app.run(host='0.0.0.0', port=8000, debug=True)
---
+++ ml/model.py
@@ -0,0 +1,108 @@
+import pickle
+
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import re
+from sklearn.model_selection import train_test_split
+import warnings
+import nltk
+from keras_preprocessing.text import Tokenizer
+from tensorflow.keras.layers import Embedding, Dense, LSTM
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.models import load_model
+from tensorflow.keras.callbacks import EarlyStopping, ModelCheckpoint
+from tensorflow.keras.preprocessing.sequence import pad_sequences
+from konlpy.tag import Okt; t = Okt()
+okt = Okt()
+warnings.filterwarnings("ignore")
+
+path = 'C:/Users/STU/Desktop/Flask-ML & DL-Example(1)/ml/total_review.csv'
+movie_review = pd.read_csv(path)
+movie_review = movie_review.dropna(axis=0)
+movie_review['review_refined'] = movie_review['review'].apply(lambda x: re.sub('[^가-힣\s]', '', x))
+movie_review = movie_review[movie_review['review_refined'].apply(lambda x: re.sub('[^가-힣]', '', x)) != '']
+movie_review = movie_review[['score', 'review', 'review_refined', 'label']]
+movie_data = movie_review[['review_refined', 'label']]
+train_review, test_review = train_test_split(movie_data, test_size=0.33, random_state=2)
+test_review.drop_duplicates(subset = ['review_refined'], inplace=True) # review 열에서 중복인 내용이 있다면 중복 제거
+test_review['review_refined'] = test_review['review_refined'].str.replace("[^ㄱ-ㅎㅏ-ㅣ가-힣 ]","") # 정규 표현식 수행
+test_review['review_refined'].replace('', np.nan, inplace=True) # 공백은 Null 값으로 변경
+test_review = test_review.dropna(how='any') # Null 값 제거
+
+stopwords = ['의','가','이','은','들','는','좀','잘','걍','과','도',
+             '를','으로','자','에','와','한','하다']
+
+X_train = []
+for sentence in train_review['review_refined']:
+    temp_X = []
+    temp_X = okt.morphs(sentence, stem=True) # 토큰화
+    temp_X = [word for word in temp_X if not word in stopwords] # 불용어 제거
+    X_train.append(temp_X)
+
+X_test = []
+for sentence in test_review['review_refined']:
+    temp_X = []
+    temp_X = okt.morphs(sentence, stem=True) # 토큰화
+    temp_X = [word for word in temp_X if not word in stopwords] # 불용어 제거
+    X_test.append(temp_X)
+
+tokenizer = Tokenizer()
+tokenizer.fit_on_texts(X_train)
+
+threshold = 3
+total_cnt = len(tokenizer.word_index) # 단어의 수
+rare_cnt = 0 # 등장 빈도수가 threshold보다 작은 단어의 개수를 카운트
+total_freq = 0 # 훈련 데이터의 전체 단어 빈도수 총 합
+rare_freq = 0 # 등장 빈도수가 threshold보다 작은 단어의 등장 빈도수의 총 합
+
+# 단어와 빈도수의 쌍(pair)을 key와 value로 받는다.
+for key, value in tokenizer.word_counts.items():
+    total_freq = total_freq + value
+
+    # 단어의 등장 빈도수가 threshold보다 작으면
+    if(value < threshold):
+        rare_cnt = rare_cnt + 1
+        rare_freq = rare_freq + value
+
+vocab_size = total_cnt - rare_cnt + 2
+tokenizer = Tokenizer(vocab_size, oov_token = 'OOV')
+tokenizer.fit_on_texts(X_train)
+
+with open('tokenizer.pickle', 'wb') as f:
+    pickle.dump(tokenizer, f, protocol=pickle.HIGHEST_PROTOCOL)
+
+X_train = tokenizer.texts_to_sequences(X_train)
+X_test = tokenizer.texts_to_sequences(X_test)
+
+y_train = np.array(train_review['label'])
+y_test = np.array(test_review['label'])
+drop_train = [index for index, sentence in enumerate(X_train) if len(sentence) < 1]
+
+X_train = np.delete(X_train, drop_train, axis=0)
+y_train = np.delete(y_train, drop_train, axis=0)
+
+def below_threshold_len(max_len, nested_list):
+  cnt = 0
+  for s in nested_list:
+    if(len(s) <= max_len):
+        cnt = cnt + 1
+
+max_len = 30
+
+X_train = pad_sequences(X_train, maxlen = max_len)
+X_test = pad_sequences(X_test, maxlen = max_len)
+
+model = Sequential()
+model.add(Embedding(vocab_size, 100))
+model.add(LSTM(128))
+model.add(Dense(1, activation='sigmoid'))
+
+es = EarlyStopping(monitor='val_loss', mode='min', verbose=1, patience=4)
+mc = ModelCheckpoint('best_model.h5', monitor='val_acc', mode='max', verbose=1, save_best_only=True)
+
+model.compile(optimizer='rmsprop', loss='binary_crossentropy', metrics=['acc'])
+history = model.fit(X_train, y_train, epochs=15, callbacks=[es, mc], batch_size=60, validation_split=0.2)
+
+# 모델 저장
+model.save('../model/best_model.h5')
