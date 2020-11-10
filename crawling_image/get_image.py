50c76cadc1d4352e1c43370623966963c1f5fafd
oyj404 <oyj404@hanmail.net>
2020-11-10 15:55:50 +0900

    wordcloud

---
+++ crawling_image/get_image.py
@@ -0,0 +1,23 @@
+import urllib.request
+from bs4 import BeautifulSoup
+import matplotlib.pyplot as plt
+from PIL import Image
+import os
+
+
+def image_poster(title_address):
+    url = f'{title_address}'
+    req = urllib.request.Request(url)
+    res = urllib.request.urlopen(url).read()
+
+    soup = BeautifulSoup(res, 'html.parser')
+    soup = soup.find("div", class_="poster")
+    # img의 경로를 받아온다
+    imgUrl = soup.find("img")["src"]
+
+    # urlretrieve는 다운로드 함수
+    # img.alt는 이미지 대체 텍스트
+    urllib.request.urlretrieve(imgUrl, soup.find("img")["alt"] + '.jpg')
+
+    plt.show()
+
